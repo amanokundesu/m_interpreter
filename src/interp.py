11776c46d6945001b27b7c3d7cbe94bcc4b9933c
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2026-08-24 00:49:20 +0000

    Edited typo in `interp.py`

--- src/interp.py
+++ src/interp.py
@@ -3,7 +3,7 @@ import io
 def run(source: str) -> str:
     output = io.StringIO()
     for line in source.splitlines():
-        exec_line(line, out)
+        exec_line(line, output)
 
 def exec_line(line:str, out):
     line = line.strip()
