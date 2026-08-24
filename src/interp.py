407d3692e3c3eb3cce056f4f2f5109f6e1bb6ba2
amanokundesu <amano.main.project@gmail.com>
2026-08-23 17:04:30 -0800

    Interp.py placeholder implementation

--- src/interp.py
+++ src/interp.py
@@ -7,7 +7,7 @@ def run(source: str) -> str:
         exec_line(line, output)
     return output.getvalue()
 
-def exec_line(line:str, out):
+def exec_line(line:str, output):
     line = line.strip()
     line = line.upper()
     if not line:
@@ -15,11 +15,14 @@ def exec_line(line:str, out):
     command, _, args = line.split(' ')
     match command[0]:
         case W:
-            write_command(args)
+            write_command(args, output)
 
 """
 The M specification says that the WRITE command has this syntax: W[RITE][:tvexpr] expr|*intexpr|fcc[,...]
 
 """
-def write_command(args: str):
-    
+def write_command(args: str, output):
+    output.write(evaluate(args))
+
+def evaluate(args:str) -> str:
+    return "Placeholder"
