a609afd20244f6f798634d854a74e8d3460bd3c0
amanokundesu <amano.main.project@gmail.com>
2026-08-23 16:49:08 -0800

    Developed first command interpretation

--- src/interp.py
+++ src/interp.py
@@ -1,13 +1,25 @@
 import io
 
+# Later add multiline support 
 def run(source: str) -> str:
     output = io.StringIO()
     for line in source.splitlines():
-        exec_line(line, out)
+        exec_line(line, output)
+    return output.getvalue()
 
 def exec_line(line:str, out):
     line = line.strip()
+    line = line.upper()
     if not line:
         return
     command, _, args = line.split(' ')
+    match command[0]:
+        case W:
+            write_command(args)
+
+"""
+The M specification says that the WRITE command has this syntax: W[RITE][:tvexpr] expr|*intexpr|fcc[,...]
+
+"""
+def write_command(args: str):
     
--- tests/first_test.py
+++ tests/first_test.py
@@ -1,4 +1,4 @@
-import repl
+import interp
 
 def test_hello():
     assert run('W 1+2') == '3'
