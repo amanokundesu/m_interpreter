c2e9ec5d2fa1d1e96e8974bedf608e019d8f970b
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2026-08-26 01:17:34 +0000

    Completed basic repl.py logic

--- src/repl.py
+++ src/repl.py
@@ -22,8 +22,34 @@ def prompt() -> str:
 # Start main loop
 loop_run:bool = True
 while loop_run:
-    prompt:str = prompt()
-    if not prompt.strip():
-        continue
-    elif prompt == "exit":
-        break
+    try:
+        line:str = prompt()
+        if not line.strip():
+            continue
+        elif line == "exit":
+            break
+        else:
+            # Execute commands run, passing into interp.py
+            # TODO: Consider exceptions later
+            try:
+                interp.run()
+            except Exception as e:
+                print(e)
+    except KeyboardInterrupt as keyboardinterrupt:
+        # TODO: Find a way to gracefully kill running processes.
+        try:
+            print(" Caught keyboard interrupt.")
+            while True:
+                temp_confirm:str = input("Confirm? [y/n]: ")
+                match temp_confirm.strip().lower():
+                    case "y"|"yes":
+                        loop_run = False
+                    case "n"|"no":
+                        print("continuing...")
+                    case _:
+                        print("Please write an accurate option")
+                        continue
+                break
+        except KeyboardInterrupt as keyboardinterrupt2:
+            print("Confirming keyboard interrupt.")
+            loop_run = False
