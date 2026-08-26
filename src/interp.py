9a955bbf4d806f2bb3c84513c4729f701b8ca96b
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2026-08-26 00:36:56 +0000

    Revised comments.

--- src/interp.py
+++ src/interp.py
@@ -1,13 +1,20 @@
 import io
 
 # Later add multiline support 
-def run(source: str) -> str:
+def run(source:str) -> str:
+    '''
+    Run the commands written in `source` parameter,
+    and returns the command run.
+    '''
     output = io.StringIO()
     for line in source.splitlines():
         exec_line(line, output)
     return output.getvalue()
 
 def exec_line(line:str, output):
+    '''
+    Executes the actual line into the output stream.
+    '''
     line = line.strip()
     line = line.upper()
     if not line:
@@ -21,7 +28,7 @@ def exec_line(line:str, output):
 The M specification says that the WRITE command has this syntax: W[RITE][:tvexpr] expr|*intexpr|fcc[,...]
 
 """
-def write_command(args: str, output):
+def write_command(args:str, output):
     output.write(evaluate(args))
 
 def evaluate(args:str) -> str:
--- tests/interp_test.py
+++ tests/interp_test.py
@@ -8,8 +8,9 @@ from pathlib import Path
 import setup_files
 from src import interp
 
-# Check that interp.run()
+# Check that interp.run() works properly.
 ## Check objectype
 assert isinstance(interp.run("test"), str), "interp.run() NOT returning STR OBJECT"
 ## Check string contents
 assert interp.run("test") == "test", "interp.run() did NOT PRINT CORRECT STRING"
+
